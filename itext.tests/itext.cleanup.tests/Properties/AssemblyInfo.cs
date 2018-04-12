9d6321273a166486d335b34c6808d5a070abcdf4
Pavel Alay <pavel.alay@duallab.com>
2018-04-12 08:20:36 +0300

    [RELEASE] 2.0.1-SNAPSHOT -> 2.0.1

--- itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.1.0")]
 [assembly: AssemblyFileVersion("2.0.1.0")]
-[assembly: AssemblyInformationalVersion("2.0.1-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
 
 [assembly: NUnit.Framework.Timeout(300000)]
