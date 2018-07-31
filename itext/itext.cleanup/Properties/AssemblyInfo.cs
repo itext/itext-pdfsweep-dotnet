9cb3bf3739dc4dbefbc7489ad5c789d8dd174532
iText Software <development@itextpdf.com>
2018-07-31 16:54:06 +0300

    [RELEASE] 2.0.2-SNAPSHOT -> 2.0.3-SNAPSHOT

--- itext/itext.cleanup/Properties/AssemblyInfo.cs
+++ itext/itext.cleanup/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ec333396-8945-4d69-b626-475b4e2ede61")]
 
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2-SNAPSHOT")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyInformationalVersion("2.0.3-SNAPSHOT")]
 
-[assembly: Versions.Attributes.KeyVersion("3.0.2.0")]
+[assembly: Versions.Attributes.KeyVersion("3.0.3.0")]
