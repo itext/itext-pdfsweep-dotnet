35238f453f42da5b56d5a330eb870b6735aadfc7
iText Software <development@itextpdf.com>
2018-10-09 16:38:24 +0300

    Update dependency versions

--- itext/itext.cleanup/Properties/AssemblyInfo.cs
+++ itext/itext.cleanup/Properties/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("2.0.3.0")]
 [assembly: AssemblyInformationalVersion("2.0.3-SNAPSHOT")]
 
-[assembly: Versions.Attributes.KeyVersion("3.0.3.0")]
+[assembly: Versions.Attributes.KeyVersion("3.0.4.0")]
