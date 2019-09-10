6117fc31c86c2ef0b2526c4a83670dc764cb9167
Kate Ivanova <ekaterina.ivanova@duallab.com>
2019-09-10 11:30:41 +0200

    Add cleanUp colorspace test
    DEVSIX-1908
    
    Autoported commit.
    Original commit hash: [48dd0b9]

---
+++ itext.tests/itext.cleanup.tests/itext/pdfcleanup/images/CleanupImageWithColorSpaceTest.cs
@@ -0,0 +1,99 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2019 iText Group NV
+Authors: iText Software.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License version 3
+as published by the Free Software Foundation with the addition of the
+following permission added to Section 15 as permitted in Section 7(a):
+FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+OF THIRD PARTY RIGHTS
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, see http://www.gnu.org/licenses or write to
+the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+Boston, MA, 02110-1301 USA, or download the license from the following URL:
+http://itextpdf.com/terms-of-use/
+
+The interactive user interfaces in modified source and object code versions
+of this program must display Appropriate Legal Notices, as required under
+Section 5 of the GNU Affero General Public License.
+
+In accordance with Section 7(b) of the GNU Affero General Public License,
+a covered work must retain the producer line in every PDF that is created
+or manipulated using iText.
+
+You can be released from the requirements of the license by purchasing
+a commercial license. Buying such a license is mandatory as soon as you
+develop commercial activities involving the iText software without
+disclosing the source code of your own applications.
+These activities include: offering paid services to customers as an ASP,
+serving PDFs on the fly in a web application, shipping iText with a closed
+source product.
+
+For more information, please contact iText Software Corp. at this
+address: sales@itextpdf.com
+*/
+using System;
+using System.Collections.Generic;
+using iText.IO.Util;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Utils;
+using iText.PdfCleanup;
+using iText.Test;
+
+namespace iText.PdfCleanup.Images {
+    public class CleanupImageWithColorSpaceTest : ExtendedITextTest {
+        private static readonly String inputPath = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/pdfcleanup/images/CleanupImageWithColorSpaceTest/";
+
+        private static readonly String outputPath = NUnit.Framework.TestContext.CurrentContext.TestDirectory + "/test/itext/pdfcleanup/images/CleanupImageWithColorSpaceTest/";
+
+        [NUnit.Framework.OneTimeSetUp]
+        public static void Before() {
+            CreateOrClearDestinationFolder(outputPath);
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        [NUnit.Framework.Ignore("test works with differs in Java and .Net, cause the one doesn't work with 8-byte images"
+            )]
+        [NUnit.Framework.Test]
+        public virtual void CleanUpTestColorSpace() {
+            // TODO: update cmp file after DEVSIX-1908 fixed
+            String input = inputPath + "imgSeparationCs.pdf";
+            String output = outputPath + "imgSeparationCs.pdf";
+            String cmp = inputPath + "cmp_imgSeparationCs.pdf";
+            CleanUp(input, output, JavaUtil.ArraysAsList(new PdfCleanUpLocation(1, new Rectangle(60f, 780f, 60f, 45f), 
+                ColorConstants.GREEN)));
+            CompareByContent(cmp, output, outputPath, "diff_imgSeparationCs");
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        private void CleanUp(String input, String output, IList<PdfCleanUpLocation> cleanUpLocations) {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(input), new PdfWriter(output));
+            PdfCleanUpTool cleaner = (cleanUpLocations == null) ? new PdfCleanUpTool(pdfDocument, true) : new PdfCleanUpTool
+                (pdfDocument, cleanUpLocations);
+            cleaner.CleanUp();
+            pdfDocument.Close();
+        }
+
+        /// <exception cref="System.IO.IOException"/>
+        /// <exception cref="System.Exception"/>
+        private void CompareByContent(String cmp, String output, String targetDir, String diffPrefix) {
+            CompareTool cmpTool = new CompareTool();
+            String errorMessage = cmpTool.CompareByContent(output, cmp, targetDir, diffPrefix + "_");
+            if (errorMessage != null) {
+                NUnit.Framework.Assert.Fail(errorMessage);
+            }
+        }
+    }
+}
