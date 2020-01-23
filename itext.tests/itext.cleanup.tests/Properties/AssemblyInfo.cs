64ee278f8d4b5bfa3dc584f3008be8296019b6de
iText Software <development@itextpdf.com>
2020-01-23 13:22:46 +0300

    [RELEASE] Update dependency versions

--- itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("647c862e-d837-4901-8e7b-68f5f8bf8f34")]
 
-[assembly: AssemblyVersion("2.0.5.0")]
-[assembly: AssemblyFileVersion("2.0.5.0")]
-[assembly: AssemblyInformationalVersion("2.0.5-SNAPSHOT")]
+[assembly: AssemblyVersion("2.0.6.0")]
+[assembly: AssemblyFileVersion("2.0.6.0")]
+[assembly: AssemblyInformationalVersion("2.0.6-SNAPSHOT")]
 
 [assembly: NUnit.Framework.Timeout(300000)]
