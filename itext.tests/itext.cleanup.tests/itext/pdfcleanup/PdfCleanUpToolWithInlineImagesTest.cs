204a26b95d2a3f698472623726a06d7739cafd9f
iText Software <development@itextpdf.com>
2020-09-13 17:48:00 +0000

    Update autoported files
    a5b5ae0e672a1ad9da8bc481228e10ac9586db57

--- itext.tests/itext.cleanup.tests/itext/pdfcleanup/PdfCleanUpToolWithInlineImagesTest.cs
+++ itext.tests/itext.cleanup.tests/itext/pdfcleanup/PdfCleanUpToolWithInlineImagesTest.cs
@@ -38,39 +38,40 @@ serving PDFs on the fly in a web application, shipping iText with a closed
 source product.
 
 For more information, please contact iText Software Corp. at this
-address: sales@itextpdf.com */
+address: sales@itextpdf.com
+*/
 using System;
 using System.Collections.Generic;
-using iText.IO;
+using NUnit.Framework;
+using iText.IO.Util;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
 using iText.Kernel.Utils;
 using iText.PdfCleanup.Util;
 using iText.Test;
 using iText.Test.Attributes;
-using NUnit.Framework;
 
 namespace iText.PdfCleanup {
     public class PdfCleanUpToolWithInlineImagesTest : ExtendedITextTest {
-
-        private static readonly String inputPath = NUnit.Framework.TestContext.CurrentContext.TestDirectory + "/../../resources/itext/pdfcleanup/PdfCleanUpToolWithInlineImagesTest/";
+        private static readonly String inputPath = iText.Test.TestUtil.GetParentProjectDirectory(NUnit.Framework.TestContext
+            .CurrentContext.TestDirectory) + "/resources/itext/pdfcleanup/PdfCleanUpToolWithInlineImagesTest/";
 
         private static readonly String outputPath = NUnit.Framework.TestContext.CurrentContext.TestDirectory + "/test/itext/pdfcleanup/PdfCleanUpToolWithInlineImagesTest/";
 
-        [NUnit.Framework.TestFixtureSetUp]
+        [NUnit.Framework.OneTimeSetUp]
         public static void Before() {
             CreateOrClearDestinationFolder(outputPath);
         }
 
         [NUnit.Framework.Test]
-        [LogMessage(LogMessageConstant.IMAGE_SIZE_CANNOT_BE_MORE_4KB)]
+        [LogMessage(iText.IO.LogMessageConstant.IMAGE_SIZE_CANNOT_BE_MORE_4KB)]
         [Ignore("DEVSIX-1617: System.Drawing.Image creates a Bitmap image object with fixed pixel format. If you try to get Graphics from such an image you'll get an exception." )]
         public virtual void CleanUpTest28() {
             String input = inputPath + "inlineImages.pdf";
             String output = outputPath + "inlineImages_partial.pdf";
             String cmp = inputPath + "cmp_inlineImages_partial.pdf";
-            IList<PdfCleanUpLocation> cleanUpLocations = iText.IO.Util.JavaUtil.ArraysAsList(new PdfCleanUpLocation(1, 
-                new Rectangle(62, 100, 20, 800), null));
+            IList<iText.PdfCleanup.PdfCleanUpLocation> cleanUpLocations = JavaUtil.ArraysAsList(new iText.PdfCleanup.PdfCleanUpLocation
+                (1, new Rectangle(62, 100, 20, 800), null));
             CleanUp(input, output, cleanUpLocations);
             CompareByContent(cmp, output, outputPath, "diff_28");
         }
@@ -81,31 +82,35 @@ namespace iText.PdfCleanup {
             String input = inputPath + "inlineImages.pdf";
             String output = outputPath + "inlineImages_partial2.pdf";
             String cmp = inputPath + "cmp_inlineImages_partial2.pdf";
-            IList<PdfCleanUpLocation> cleanUpLocations = iText.IO.Util.JavaUtil.ArraysAsList(new PdfCleanUpLocation(1, 
-                new Rectangle(10, 100, 70, 599), null));
+            IList<iText.PdfCleanup.PdfCleanUpLocation> cleanUpLocations = JavaUtil.ArraysAsList(new iText.PdfCleanup.PdfCleanUpLocation
+                (1, new Rectangle(10, 100, 70, 599), null));
             CleanUp(input, output, cleanUpLocations);
             CompareByContent(cmp, output, outputPath, "diff_29");
         }
 
         [NUnit.Framework.Test]
-        [LogMessage(LogMessageConstant.IMAGE_SIZE_CANNOT_BE_MORE_4KB)]
+        [LogMessage(iText.IO.LogMessageConstant.IMAGE_SIZE_CANNOT_BE_MORE_4KB)]
         public virtual void CleanUpTest31() {
             String input = inputPath + "inlineImageCleanup.pdf";
             String output = outputPath + "inlineImageCleanup.pdf";
             String cmp = inputPath + "cmp_inlineImageCleanup.pdf";
             CleanUp(input, output, null);
-            CleanUpImagesCompareTool compareTool = new CleanUpImagesCompareTool();
-            string errorMessage = compareTool.ExtractAndCompareImages(cmp, output, outputPath, "1");
-            errorMessage += compareTool.CompareByContent(output, cmp, outputPath);
+            CleanUpImagesCompareTool cmpTool = new CleanUpImagesCompareTool();
+            String errorMessage = cmpTool.ExtractAndCompareImages(output, cmp, outputPath, "1");
+            String compareByContentResult = cmpTool.CompareByContent(output, cmp, outputPath);
+            if (compareByContentResult != null) {
+                errorMessage += compareByContentResult;
+            }
             if (!errorMessage.Equals("")) {
-                Assert.Fail(errorMessage);
+                NUnit.Framework.Assert.Fail(errorMessage);
             }
         }
 
-        private void CleanUp(String input, String output, IList<PdfCleanUpLocation> cleanUpLocations) {
+        private void CleanUp(String input, String output, IList<iText.PdfCleanup.PdfCleanUpLocation> cleanUpLocations
+            ) {
             PdfDocument pdfDocument = new PdfDocument(new PdfReader(input), new PdfWriter(output));
-            PdfCleanUpTool cleaner = (cleanUpLocations == null) ? new PdfCleanUpTool(pdfDocument, true) : new PdfCleanUpTool
-                (pdfDocument, cleanUpLocations);
+            iText.PdfCleanup.PdfCleanUpTool cleaner = (cleanUpLocations == null) ? new iText.PdfCleanup.PdfCleanUpTool
+                (pdfDocument, true) : new iText.PdfCleanup.PdfCleanUpTool(pdfDocument, cleanUpLocations);
             cleaner.CleanUp();
             pdfDocument.Close();
         }
