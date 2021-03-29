aea56622bb82cac5470c5f06b04283e0d854e95e
iText Software <development@itextpdf.com>
2021-03-29 15:19:08 +0300

    [AFTER RELEASE] pdfSweep 2.0.6

--- itext/itext.cleanup/Properties/AssemblyInfo.cs
+++ itext/itext.cleanup/Properties/AssemblyInfo.cs
@@ -21,8 +21,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("ec333396-8945-4d69-b626-475b4e2ede61")]
 
-[assembly: AssemblyVersion("2.0.6.0")]
-[assembly: AssemblyFileVersion("2.0.6.0")]
-[assembly: AssemblyInformationalVersion("2.0.6-SNAPSHOT")]
+[assembly: AssemblyVersion("2.0.7.0")]
+[assembly: AssemblyFileVersion("2.0.7.0")]
+[assembly: AssemblyInformationalVersion("2.0.7-SNAPSHOT")]
 
-[assembly: Versions.Attributes.KeyVersion("3.1.3.0")]
+[assembly: Versions.Attributes.KeyVersion("3.1.4.0")]
