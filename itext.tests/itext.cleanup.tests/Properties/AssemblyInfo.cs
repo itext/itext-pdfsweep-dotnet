cefa1949db0d9ef0acc991b1be09c9810615cd34
iText Software <development@itextpdf.com>
2022-12-07 13:40:18 +0300

    [RELEASE] pdfSweep 2.0.8

--- itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.8.0")]
 [assembly: AssemblyFileVersion("2.0.8.0")]
-[assembly: AssemblyInformationalVersion("2.0.8-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("2.0.8")]
 
 [assembly: NUnit.Framework.Timeout(300000)]
