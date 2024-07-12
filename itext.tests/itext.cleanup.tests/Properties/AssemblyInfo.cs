d446958e1870320f8f97653f044fa9feeaf35746
Apryse Software <development@apryse.com>
2024-07-12 16:34:25 +0000

    [AFTER RELEASE] pdfSweep 4.0.3

--- itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
+++ itext.tests/itext.cleanup.tests/Properties/AssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("647c862e-d837-4901-8e7b-68f5f8bf8f34")]
 
-[assembly: AssemblyVersion("4.0.3.0")]
-[assembly: AssemblyFileVersion("4.0.3.0")]
-[assembly: AssemblyInformationalVersion("4.0.3-SNAPSHOT")]
+[assembly: AssemblyVersion("4.0.4.0")]
+[assembly: AssemblyFileVersion("4.0.4.0")]
+[assembly: AssemblyInformationalVersion("4.0.4-SNAPSHOT")]
 
 [assembly: NUnit.Framework.Timeout(300000)]
